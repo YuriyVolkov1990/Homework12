3ff1d41a1ff06417cc6f065fc06d398cb6dc2a3d
Professional <new99kpicem@gmail.com>
2023-03-11 01:45:39 +0300

    training

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book warAndPeace = new Book();
+
+        warAndPeace.authorName = "Л.Н.Толстой";
+        System.out.println("Автор - " + warAndPeace.authorName);
+
+        warAndPeace.bookName = "Война и Мир";
+        System.out.println("Название - " + warAndPeace.bookName);
+        warAndPeace.publishingYear = 1867;
+        System.out.println("Год - " + warAndPeace.publishingYear);
     }
+    public static class Book {
+        String authorName;
+        String bookName;
+        int publishingYear;
+        }
 }
