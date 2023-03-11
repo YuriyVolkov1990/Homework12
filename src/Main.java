87c90f35b27dc467e022ad48bac17eaf1f8b714e
Professional <new99kpicem@gmail.com>
2023-03-11 10:56:06 +0300

    training

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,31 @@ public class Main {
 
         warAndPeace.publishingYear = 1867;
         System.out.println("Год - " + warAndPeace.publishingYear);
+
+        System.out.println();
+        Person sarah = new Person();
+        // Тип переменной — Person (Личность)
+        sarah.name = "Sarah";
+        // Внутри свойства name записали имя
+        System.out.println("sarah.name = " + sarah.name);
+        // Вывели свойство «имя» в консоль
+        sarah.age = 30;
+        // Внутри свойства age записали возраст
+        System.out.println("sarah.age = " + sarah.age);
+        // Вывели свойство «возраст» в консоль
     }
-    public static class Book {
+
+    public static class Person {
+        // Создали новый класс Person (Личность)
+        String name;
+        // Объявили свойство name (имя)
+        int age;
+        // Объявили свойство age (возраст)
+    }
+}
+    class Book {
         String authorName;
         String bookName;
         int publishingYear;
-        }
-}
+    }
+
