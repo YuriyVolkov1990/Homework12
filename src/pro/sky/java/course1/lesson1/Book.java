671b104cf35c4d2a36aea88c54af92d961fbcb4b
Professional <new99kpicem@gmail.com>
2023-03-12 19:46:02 +0300

    hw

--- src/pro/sky/java/course1/lesson1/Book.java
+++ src/pro/sky/java/course1/lesson1/Book.java
@@ -1,10 +1,26 @@
 package pro.sky.java.course1.lesson1;
 
 public class Book {
-    String bookName;
-    Author createAuthor;
-    int yearPublishing;
+    private String bookName;
+    private Author createAuthor;
+    private int yearPublishing;
+    public Book(String bookName, Author createAuthor, int yearPublishing) {
+        this.bookName = bookName;
+        this.createAuthor = createAuthor;
+        this.yearPublishing = yearPublishing;
+    }
 
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public Author getCreateAuthor() {
+        return this.createAuthor;
+    }
+
+    public int getYearPublishing() {
+        return this.yearPublishing;
+    }
     public static Book createBook(String bookName, Author authorName, int yearPublishing) {
         Book book = new Book();
         book.bookName = bookName;
