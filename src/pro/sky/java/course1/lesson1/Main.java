d84dcf17af4110f5b7d796c48a8e11e1e1fbab18
Professional <new99kpicem@gmail.com>
2023-03-12 19:06:14 +0300

    hw

--- src/pro/sky/java/course1/lesson1/Main.java
+++ src/pro/sky/java/course1/lesson1/Main.java
@@ -1,6 +1,14 @@
 package pro.sky.java.course1.lesson1;
-
+import java.util.Scanner;
 
 public class Main {
-
+    public static void main() {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Имя автора:");
+        String name = n.nextLine();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Фамилия автора:");
+        String surname = s.nextLine();
+        Author book = Author.createAuthor(name, surname);
+    }
 }
