fecde25b7af176f5d27beb77de57bf8721ede5a7
Professional <new99kpicem@gmail.com>
2023-03-12 18:51:04 +0300

    hw

--- src/pro/sky/java/course1/lesson1/Author.java
+++ src/pro/sky/java/course1/lesson1/Author.java
@@ -3,4 +3,12 @@ package pro.sky.java.course1.lesson1;
 public class Author {
     String authorName;
     String authorSurname;
+
+    public static Author createAuthor(String authorName, String authorSurname) {
+        Author author = new Author();
+        author.authorName = authorName;
+        author.authorSurname = authorSurname;
+        return author;
+    }
+
 }
--- src/pro/sky/java/course1/lesson1/Main.java
+++ src/pro/sky/java/course1/lesson1/Main.java
@@ -3,10 +3,4 @@ package pro.sky.java.course1.lesson1;
 
 public class Main {
 
-    public static Author createAuthor(String authorName, String authorSurname) {
-        Author author = new Author();
-        author.authorName = authorName;
-        author.authorSurname = authorSurname;
-        return author;
-    }
 }
