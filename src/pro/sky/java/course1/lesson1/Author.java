4d695ef3a97bed3f97af730fc2c7ea539263cbd7
Professional <new99kpicem@gmail.com>
2023-03-12 20:18:13 +0300

    hw

--- src/pro/sky/java/course1/lesson1/Author.java
+++ src/pro/sky/java/course1/lesson1/Author.java
@@ -1,8 +1,12 @@
 package pro.sky.java.course1.lesson1;
 
 public class Author {
-    String authorName;
-    String authorSurname;
+    private String authorName;
+    private String authorSurname;
+    public Author(String authorName,String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+    }
 
 
     public static Author createAuthor(String authorName, String authorSurname) {
